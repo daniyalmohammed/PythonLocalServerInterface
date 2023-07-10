ff0ed578ca0a2a1de9af3dee178522c23331513b
Daniyal Mohammed <d25moham@uwaterloo.ca>
2023-07-10 16:10:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ class ChannelId(Enum):
     ACQ_CHANNEL_6 = 1 << 5
     ACQ_CHANNEL_7 = 1 << 6
     ACQ_CHANNEL_8 = 1 << 7
+    ACQ_CHANNEL_9 = 1 << 8
+    ACQ_CHANNEL_10 = 1 << 9
+    ACQ_CHANNEL_11 = 1 << 10
+    ACQ_CHANNEL_12 = 1 << 11
+    ACQ_CHANNEL_13 = 1 << 12
+    ACQ_CHANNEL_14 = 1 << 13
+    ACQ_CHANNEL_15 = 1 << 14
+    ACQ_CHANNEL_16 = 1 << 15
 
 channels = [
     ChannelId.ACQ_CHANNEL_1,
@@ -20,14 +28,22 @@ channels = [
     ChannelId.ACQ_CHANNEL_3,
     ChannelId.ACQ_CHANNEL_4,
     ChannelId.ACQ_CHANNEL_5,
-    ChannelId.ACQ_CHANNEL_6
+    ChannelId.ACQ_CHANNEL_6,
+    ChannelId.ACQ_CHANNEL_7,
+    ChannelId.ACQ_CHANNEL_8,
+    ChannelId.ACQ_CHANNEL_9,
+    ChannelId.ACQ_CHANNEL_10,
+    ChannelId.ACQ_CHANNEL_11,
+    ChannelId.ACQ_CHANNEL_12,
+    ChannelId.ACQ_CHANNEL_13,
+    ChannelId.ACQ_CHANNEL_14,
+    ChannelId.ACQ_CHANNEL_15,
+    ChannelId.ACQ_CHANNEL_16,
 ]
 
 class ChannelIdEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ChannelId):
-            print(obj.name)
-            print(obj.value)
             return obj.name
         return super().default(obj)
 
@@ -41,10 +57,11 @@ def configure():
         entry_text = entries[i].get()
         if not entry_text:  # Check if the field is empty
             errorMessage = "Please fill in the " + labels[i]
-            tk.messagebox.showwarning("Field Empty",errorMessage[:len(errorMessage) - 1])
+            tk.messagebox.showwarning("Field Empty", errorMessage[:len(errorMessage) - 1])
             return
         data[label_text.lower()] = entry_text
     data["channel_format"] = int(channel_format_var.get())
+    data["channel_count"] = int(channel_count_var.get())
     
     json_data = {
         "messageType": "LSL_OUTLET_CONFIG",
@@ -76,7 +93,7 @@ def start():
     data["samplingRateHz"] = entries[3].get()
     data["sessionToken"] = 1
     data["samplesPerEpoch"] = 64
-    data["enabledChannels"] = [channel.name for channel in channels]
+    data["enabledChannels"] = [channel.name for channel in channels[:int(channel_count_var.get())]]
     data["measureImpedance"] = False
     
     json_data = {
@@ -135,9 +152,9 @@ device_entry = tk.Entry(window)
 device_entry.pack()
 
 # Create the input fields and labels
-labels = ['Name:', 'Type:', 'Channel Count:', 'Nominal SRate:', 'Source ID:']
-requestLabels = ['name', 'type', 'channel_count', 'nominal_srate', 'source_id']
-defaultValues = ['boneshaker', 'EEG', 6, 250, 'CXN_UID']
+labels = ['Name:', 'Type:', 'Nominal SRate:', 'Source ID:']
+requestLabels = ['name', 'type', 'nominal_srate', 'source_id']
+defaultValues = ['boneshaker', 'EEG', '250', 'CXN_UID']
 entries = []
 
 for i, label_text in enumerate(labels):
@@ -159,6 +176,15 @@ channel_format_var = tk.StringVar(window)
 channel_format_var.set("1")  # Default value is set to 1
 channel_format_dropdown = tk.OptionMenu(window, channel_format_var, *range(1, 8))
 channel_format_dropdown.pack()
+
+# Create the channel count dropdown menu
+channel_count_label = tk.Label(window, text='Channel Count:')
+channel_count_label.pack()
+
+channel_count_var = tk.StringVar(window)
+channel_count_var.set("6")  # Default value is set to 6
+channel_count_dropdown = tk.OptionMenu(window, channel_count_var, *range(1, 17))
+channel_count_dropdown.pack()
 
 # Create the configure button
 configure_button = tk.Button(window, text="Configure", bg="yellow", command=configure)
